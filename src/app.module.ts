d44e540e4cc692d463d31d21253aaccbab1c3414
Melah Ghislain <melahghislain17@gmail.com>
2022-10-23 22:10:43 +0100

    initialised entity classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
 import { ReviewModule } from './review/review.module';
 import { CartModule } from './cart/cart.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
@@ -16,8 +16,9 @@ import { CartModule } from './cart/cart.module';
     OrderModule,
     ReviewModule,
     CartModule,
+    TypeOrmModule.forRoot(typeormConfig),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
